2f2c48990ba433b1402d9ab58371f53cf2a90a35
Abaddon-the-despoiler <sammifr114@gmail.com>
2026-05-11 20:00:30 +0200

    4h commit

---
+++ src/frontend/dashboard.php
@@ -0,0 +1,35 @@
+<?php
+
+
+?>
+
+
+
+<html>
+    <head>
+        <link rel="stylesheet" href="styles/dashboard.css">
+    </head>
+
+
+
+<body>
+<nav>
+    <img src="images/logo.png" alt="logo vacantie blog">
+
+   
+        <ul>
+        <li><a href="default.asp">Home</a></li>
+        <li><a href="news.asp">News</a></li>
+        <li><a href="contact.asp">Contact</a></li>
+        <li><a href="about.asp">About</a></li>
+        </ul>
+
+</nav>
+<button type="button" class="btn">
+    Klik hier
+</button>
+<div class="overzicht">
+
+</div>
+
+</body>
